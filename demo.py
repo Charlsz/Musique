4a4d4a7125b8ed76d214b0ef4d948593149e6eec
Charls <cgalvis21_@hotmail.com>
2022-11-12 14:03:55 -0500

    new code

--- demo.py
+++ demo.py
@@ -1,21 +1,37 @@
-from tkinter import *
-import pygame 
-  
-root = Tk() 
-root.title('GeeksforGeeks sound player') 
-  
-root.geometry("500x400") 
-  
-pygame.mixer.init()
-  
-def play(): 
-    pygame.mixer.music.load("1.mp3") 
-    pygame.mixer.music.play(loops=0) 
-  
-title=Label(root,text="Musique",bd=9,relief=GROOVE, 
-            font=("times new roman",50,"bold"),bg="white",fg="green")  
-title.pack(side=TOP,fill=X)  
-  
-play_button = Button(root, text="Play Song", font=("Helvetica", 32), command=play) 
-play_button.pack(pady=20) 
-root.mainloop()
+import tkinter as tkr
+from playsound import playsound
+from tkinter.filedialog import askdirectory
+import os
+
+musique = tkr.Tk()
+musique.title("Music Player")
+musique.geometry("450x350")
+
+def play():
+    playsound("joji.mp3")
+
+def ExitMusicPlayer():
+    pass
+
+def pause():
+    playsound(None)
+
+def unpause():
+    pass
+
+Button1 = tkr.Button(musique,width=5,height=3, font="Helvetica 12 bold",text="PLAY",command=play,bg="red",fg="white")
+Button2 = tkr.Button(musique,width=5,height=3, font="Helvetica 12 bold",text="STOP",command=ExitMusicPlayer,bg="purple",fg="white")
+Button3 = tkr.Button(musique,width=5,height=3, font="Helvetica 12 bold",text="PAUSE",command=pause,bg="green",fg="white")
+Button4 = tkr.Button(musique,width=5,height=3, font="Helvetica 12 bold",text="UNPAUSE",command=unpause,bg="blue",fg="white")
+
+
+var = tkr.StringVar()
+songtitle = tkr.Label(musique, font="Helvetica 12 bold", textvariable=var)
+
+songtitle.pack()
+Button1.pack(fill="x")
+Button2.pack(fill="x")
+Button3.pack(fill="x")
+Button4.pack(fill="x")
+
+musique.mainloop()
