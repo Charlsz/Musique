9f43c34e0ab6fe5b1f4b69c975272c20a90249b8
Charls <cgalvis21_@hotmail.com>
2022-11-13 16:19:07 -0500

    update user

---
+++ user.py
@@ -0,0 +1,49 @@
+class usuario():
+
+    numUsuarios = 0
+    
+    def __init__(self,nombre,contra):
+        self.nombre = nombre
+        self.contra = contra
+
+        self.conectado = False
+        self.intentos = 3
+
+        usuario.numUsuarios=+1
+
+    def conectar(self):
+        myContra = input("ingresa tu password: ")
+        if myContra==self.contra:
+            print("se ha conectado")
+            self.conectado = True
+        else:
+            self.intentos-=1
+            if self.intentos > 0:
+                print("password incorrecta, intentelo otra vez: ")
+                print("intentos restantes: ", self.intentos)
+                self.conectar()
+            else:
+                print("No se pudo iniciar sesion, hasta luego")
+    
+    def desconectar(self):
+        if self.conectado:
+            print("se ha cerrado sesion")
+            self.conectado = False
+        else:
+            print("No ha iniciado sesion")
+    
+    def __str__(self):
+        if self.conectado:
+            conect = "conectado"
+        else:
+            conect = "desconectado"
+        return f"mi nombre de usuario es {self.nombre} y estoy {conect}"
+
+user1 = usuario(input("escribe tu usuario: "),input("escribe tu password: "))
+print(user1)
+
+user1.conectar()
+print(user1)
+
+user1.desconectar()
+print(user1)
