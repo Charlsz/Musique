cc473fda77ef013610d0dbc787c0b246a9a6a1d1
Charls <cgalvis21_@hotmail.com>
2022-11-12 19:46:12 -0500

    botones nuevos, ajustar open folder

--- demo.py
+++ demo.py
@@ -1,37 +1,55 @@
 import tkinter as tkr
 from playsound import playsound
-from tkinter.filedialog import askdirectory
+from tkinter import *
+from tkinter import Tk
+from tkinter import filedialog
 import os
 
 musique = tkr.Tk()
-musique.title("Music Player")
+musique.title("Musique")
 musique.geometry("450x350")
 
+def AddMusic():
+    path = filedialog.askdirectory()
+    if path:
+        os.chdir(path)
+        songs = os.listdir(path)
+        for song in songs:
+            if song.endswith(".mp3"):
+                Playlist.insert(END, song)
+
+
 def play():
+    Music_Name = Playlist.get(ACTIVE)
+    print(Music_Name[0:-4])
     playsound("joji.mp3")
 
 def ExitMusicPlayer():
     pass
 
 def pause():
-    playsound(None)
+    pass
 
 def unpause():
     pass
 
-Button1 = tkr.Button(musique,width=5,height=3, font="Helvetica 12 bold",text="PLAY",command=play,bg="red",fg="white")
-Button2 = tkr.Button(musique,width=5,height=3, font="Helvetica 12 bold",text="STOP",command=ExitMusicPlayer,bg="purple",fg="white")
-Button3 = tkr.Button(musique,width=5,height=3, font="Helvetica 12 bold",text="PAUSE",command=pause,bg="green",fg="white")
-Button4 = tkr.Button(musique,width=5,height=3, font="Helvetica 12 bold",text="UNPAUSE",command=unpause,bg="blue",fg="white")
 
+
+
+
+Button1 = tkr.Button(musique,width=3,height=2, font="Arial",text="PLAY",command=play,bg="blue",fg="white")
+Button(musique, text="Open Folder", width=10, height=3, font=("Arial"), fg="Purple", bg="#21b3de", command=AddMusic).place(x="10",y="40")
+
+Scroll = Scrollbar(musique)
+Playlist = Listbox(musique, width=5,height=3, font=("Arial"), bg="#333333", fg="grey", selectbackground="lightblue", cursor="hand2", bd=0, yscrollcommand=Scroll.set)
+Scroll.config(command=Playlist.yview)
+Scroll.pack(side=RIGHT, fill=Y)
 
 var = tkr.StringVar()
 songtitle = tkr.Label(musique, font="Helvetica 12 bold", textvariable=var)
 
 songtitle.pack()
 Button1.pack(fill="x")
-Button2.pack(fill="x")
-Button3.pack(fill="x")
-Button4.pack(fill="x")
+Playlist.pack(fill="x")
 
 musique.mainloop()
